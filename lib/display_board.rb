bd6953b4ebc23e232743c13bfa6428e66aefde58
Parmanand Sahadeo <parmsahadeo530@gmail.com>
2016-07-05 18:57:47 +0000

    Wrote body for #display_board

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
+def display_board(array)
+  rows = []
+  rows << " #{array[0]} | #{array[1]} | #{array[2]} "
+  rows << "-----------"
+  rows << " #{array[3]} | #{array[4]} | #{array[5]} "
+  rows << "-----------"
+  rows << " #{array[6]} | #{array[7]} | #{array[8]} "
+  puts rows
 end
